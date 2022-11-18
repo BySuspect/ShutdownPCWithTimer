4e6c3f946e7f5e5684f34c5f807f273a7788e7a2
BySuspect <omerbayraktar43@gmail.com>
2022-11-18 17:08:43 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using RemoteShutdownPC.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace RemoteShutdownPC
+{
+    public partial class Form1 : Form
+    {
+        FirebaseHelper firebase = new FirebaseHelper();
+        bool threadRunning = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //Starting background Thread
+            Thread thread = new Thread(backgroundTask);
+            thread.SetApartmentState(ApartmentState.STA);
+            CheckForIllegalCrossThreadCalls = false;
+            thread.Start();
+        }
+
+        async void backgroundTask()
+        {
+            while (threadRunning)
+            {
+                Thread.Sleep(49);//Minimum CPU usage
+                var list = await firebase.GetData("Shutdown");//Requesting data from firebase
+                if (list.Shutdown)
+                {
+                    if (list.Timer != "0")//If shutdowning with timer
+                    {
+                        await sendToCMD(Convert.ToInt32(list.Timer));
+                        firebase.UpdateData(list.Name, false, "0", list.Cancel);
+                    }
+                    else
+                    {//Without timer shutdown sending 1 milisecond
+                        await sendToCMD(1);
+                        //Console.WriteLine("Shutdown");
+                    }
+                }
+
+                if (list.Cancel)//if cancel is true 
+                {
+                    await cancelShutdown();
+                    firebase.UpdateData(list.Name, list.Shutdown, "0", false);
+                }
+            }
+        }
+
+        //Send shutdown timer to CMD
+        async Task sendToCMD(int time)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -s -t " + time;//Time is milisecond
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        //Send cancel command to CMD
+        async Task cancelShutdown()
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ Helper/FirebaseHelper.cs
@@ -0,0 +1,86 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteShutdownPC.Helper
+{
+    public class FirebaseHelper
+    {
+        const string auth = "~Firebase Secret~";
+        public static FirebaseClient firebase = new FirebaseClient("https://~Your Firebase Url~.firebaseio.com/",
+            new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(auth)
+            });
+        public static string mainChild = "Main";
+        public FirebaseHelper()
+        {
+
+        }
+
+        //Get data from firebase
+        public async Task<FBDatas> GetData(string name)
+        {
+            try
+            {
+                var AllData = await firebase
+                 .Child(mainChild)
+                 .OnceAsync<FBDatas>();
+
+                var list = AllData.Select(item => new FBDatas
+                {
+                    Name = item.Object.Name,
+                    Shutdown = item.Object.Shutdown,
+                    Timer = item.Object.Timer,
+                    Cancel = item.Object.Cancel,
+                }).ToList();
+
+                return new FBDatas
+                {
+                    Name = list[0].Name,
+                    Timer = list[0].Timer,
+                    Shutdown = list[0].Shutdown,
+                    Cancel = list[0].Cancel,
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //Update data on firebase
+        public async void UpdateData(string name, bool shutdown, string timer, bool cancel)
+        {
+            try
+            {
+                await firebase
+                    .Child(mainChild)
+                    .Child(name)
+                    .PutAsync(new FBDatas() { Name = name, Shutdown = shutdown, Timer = timer, Cancel = cancel });
+            }
+            catch (Exception) { }
+        }
+
+        //Add new data to firebase
+        public async Task AddData(string name)
+        {
+            await firebase
+                .Child(mainChild)
+                .Child(name)
+                .PutAsync(new FBDatas() { Name = name, Shutdown = false, Timer = "0", Cancel = false });
+        }
+    }
+
+    public class FBDatas //Firebase Datas
+    {
+        public string Name { get; set; }
+        public bool Shutdown { get; set; }
+        public string Timer { get; set; }
+        public bool Cancel { get; set; }
+    }
+}
